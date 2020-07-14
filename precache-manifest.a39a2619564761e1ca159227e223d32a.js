5460ab1382ba2486bd502ab1cdeadc07543e60c2
Sandip Gahlot <sandip.gahlot@capco.com>
2020-07-13 23:42:18 -0400

    Updates

---
+++ precache-manifest.a39a2619564761e1ca159227e223d32a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4889c8ba4ea38066cb495811541c72b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f8e31178bb75e170758b",
+    "url": "/monsters-rolodex/static/css/main.3b9ae2f5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e31178bb75e170758b",
+    "url": "/monsters-rolodex/static/js/main.865c7b4c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
